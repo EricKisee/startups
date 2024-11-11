02df0d0fe161d6db63cf1fc52f45e45c39f5642c
EricKisee <erickiseemulwa@gmail.com>
2024-11-11 16:57:05 +0300

    Monday 11-11

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,29 @@
 import Image from "next/image";
-import SearchForm from "../../components/SearchForm";
+import SearchForm from "@/components/SearchForm";
+import StartupCard, {StartupTypeCard} from "@/components/StartupCard";
+import { client } from "@/sanity/lib/client";
+import { STARTUPS_QUERY } from "@/sanity/lib/queries";
 
 export default async function Home( {searchParams} : {
   searchParams: Promise<{query?: string}>
 } ) {
   const query = (await searchParams).query
+
+  const posts = await client.fetch(STARTUPS_QUERY)
+
+  const postss = [
+    {
+      _createdAt: new Date(),
+      views: 55,
+      author: { _id:1, name: 'Eric' },
+      _id: 1,
+      description: "This is a description",
+      image: "https://images.unsplash.com/photo-1589254065909-b7086229d08c?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      category: "Robots",
+      title: "We Robots",
+    },
+  ]
+
   return (
     <>
       <section className="pink_container">
@@ -12,6 +31,22 @@ export default async function Home( {searchParams} : {
         <p className="sub-heading !max-w-3xl">Submit Ideas, Vote on Pitches, and Get Noticed in Virtual Competitions</p>
 
         <SearchForm query={query} />
+      </section>
+
+      <section className="section_container">
+        <p className="text-30-semibold">
+          { query ? `Search results for "${query}"` : 'All Startups'}
+        </p>
+
+        <ul className="mt-7 card_grid">
+          { posts?.length>0 ? (
+            posts.map((post: StartupTypeCard )=>(
+              <StartupCard key={post?._id} post={post} />
+            ))
+          ) : (
+            <p className="no-results">No startups found</p>
+          )}
+        </ul>
       </section>
 
     </>
