04cf180581fd486ad71c464b9622ae97e56f0001
EricKisee <erickiseemulwa@gmail.com>
2024-11-12 17:00:52 +0300

    12-11-2024

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -3,26 +3,19 @@ import SearchForm from "@/components/SearchForm";
 import StartupCard, {StartupTypeCard} from "@/components/StartupCard";
 import { client } from "@/sanity/lib/client";
 import { STARTUPS_QUERY } from "@/sanity/lib/queries";
+import { sanityFetch, SanityLive } from "@/sanity/lib/live";
+import { auth } from "@/auth";
 
 export default async function Home( {searchParams} : {
   searchParams: Promise<{query?: string}>
 } ) {
   const query = (await searchParams).query
+  const params = {search: query || null}
 
-  const posts = await client.fetch(STARTUPS_QUERY)
+  const session = await auth()
+  console.log(session?.id)
 
-  const postss = [
-    {
-      _createdAt: new Date(),
-      views: 55,
-      author: { _id:1, name: 'Eric' },
-      _id: 1,
-      description: "This is a description",
-      image: "https://images.unsplash.com/photo-1589254065909-b7086229d08c?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-      category: "Robots",
-      title: "We Robots",
-    },
-  ]
+  const {data: posts} = await sanityFetch({query: STARTUPS_QUERY, params})
 
   return (
     <>
@@ -48,7 +41,7 @@ export default async function Home( {searchParams} : {
           )}
         </ul>
       </section>
-
+      <SanityLive />
     </>
   );
 }
